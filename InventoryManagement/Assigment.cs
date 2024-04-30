e4ec146aeaefd595c1a1e4a39b069ae4e5729fbd
Yunus Emre Akkaş <58690489+yunusemreakkas16@users.noreply.github.com>
2024-04-30 10:41:00 +0300

    The project has been set up, classes and objects have been added

---
+++ InventoryManagement/Assigment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    internal class Assigment
+    {
+        public int AssigmentId { get; set; }
+        public DateTime AssigmentDate { get; set; } //date!
+        public DateTime AssigmentEndDate { get; set; } //date!
+        public int FK_PersonnelId {  get; set; }
+        public int FK_ItemId { get; set; }
+    }
+}
