9cbe7d9f26da72db25e90a325345e296fa2fe65e
Yunus Emre Akkaş <58690489+yunusemreakkas16@users.noreply.github.com>
2024-04-30 14:01:46 +0300

    Added a data using with dapper

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,6 +20,19 @@ namespace InventoryManagement
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            using(IDbConnection connection = new SqlConnection("Data Source=NEUTRONSTAR\\SQLEXPRESS;Initial Catalog=INVENTORY;Integrated Security=True;Encrypt=False"))
+            {
+                connection.Open();
+                //Add Data
+                connection.Execute(
+                    "insert into Personnel(PersonnelName,PersonnelSurname,PersonnelDepartment) values(@PersonnelName,@PersonnelSurname,@PersonnelDepartment)",
+                    new Personnel { PersonnelName = "Jane", PersonnelSurname = "Smith", PersonnelDepartment ="IT" }
+                    );
+                //Get Data
+                List<Personnel> personnelsList = connection.Query<Personnel>("select * from Personnel").ToList();
+                connection.Close();
+            }
         }
     }
 }
